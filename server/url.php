7634e9792201f7ab4bd78e5f7002df448bad2eab
liangjian <liangjian@oliveche.com>
2021-11-04 14:10:23 +0800

    merge from wms: 支持jdcloud v6

--- server/url.php
+++ server/url.php
@@ -1,30 +1,20 @@
 <?php
 
-require_once('app.php');
-
-class UrlApp extends AppBase
-{
-	protected function onExec()
-	{
-		$p = mparam("p");
-		$param = json_decode(jdEncrypt($p, "D"), true);
-		if (!$param) {
-			throw new MyException(E_PARAM);
-		}
-		$_GET = $param["get"];
-		$_POST = $param["post"];
-		if ($param["ses"]) {
-			session_id($param["ses"]);
-		}
-		// fix APP
-		if (@$param["get"]["_app"]) {
-			global $APP;
-			$APP = $param["get"]["_app"];
-			session_name($APP . "id");
-		}
-	}
-}
-
-$app = new UrlApp();
-$ret = $app->exec();
 require_once("api.php");
+
+$env = getJDEnv();
+$env->onBeforeActions[] = function () {
+	$p = mparam("p");
+	$param = jsonDecode(jdEncrypt($p, "D"));
+	if (!$param) {
+		logit("url.php: error param p=" . $p);
+		jdRet(E_PARAM);
+	}
+	$_GET = $param["get"];
+	$_POST = $param["post"];
+	if ($param["ses"]) {
+		session_id($param["ses"]);
+	}
+};
+
+callSvc();
